a14b91954893ab262153096a006e763b4f5ebecf
poshansahu <sahuposhan610@gmail.com>
2025-01-08 18:22:14 +0530

    Changes Pages Layout

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,6 @@
-import About from "../components/About"
-const AboutPage = ()=>{
-    return(
-        <About/>
-    );
-}
+import About from "../components/About";
+const AboutPage = () => {
+  return <About />;
+};
 
 export default AboutPage;
