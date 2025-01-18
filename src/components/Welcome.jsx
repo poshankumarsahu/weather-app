979c5edd27e307904e186cf869b9d240a1856334
poshansahu <sahuposhan610@gmail.com>
2025-01-18 16:24:43 +0530

    Added go to weather report button

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 const Welcome = () => {
+  const navigate = useNavigate(); // Hook for navigation
+
+  const handleClick = () => {
+    navigate("/weather"); // Navigate to the Weather component
+  };
+
   return (
     <div className="page-content">
       <h1>Welcome to Weather App</h1>
       <p>Discover accurate weather information at your fingertips.</p>
+      <button onClick={handleClick} style={{ padding: "10px" }}>
+        Go To Weather
+      </button>
     </div>
   );
 };
